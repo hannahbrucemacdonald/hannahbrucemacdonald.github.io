44c01babce3f3f108eb1749dfd884798e8c38976
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2019-06-04 21:26:47 -0400

    updating asthetics

--- generate_network.py
+++ generate_network.py
@@ -3,11 +3,12 @@ import networkx as nx
 import pandas as pd
 from collections import Counter
 
-net = Network(height="750px", width="100%", bgcolor="white", font_color="black",directed=True)
+net = Network(height="1000px", width="100%",directed=True,bgcolor="#222222", font_color="white")
 
 # set the physics layout of the network
 net.barnes_hut()
-data = pd.read_csv("themob.csv")
+net.force_atlas_2based()
+data = pd.read_csv("details.csv",encoding = "ISO-8859-1", error_bad_lines=False) 
 
 people = data['Person']
 supervisors = data['Supervisor']
@@ -16,10 +17,10 @@ jobtype = data['Type']
 color = {'PhD' : 'blueviolet' , 'postdoc' : 'magenta'}
 
 all_people = set(list(people)+list(supervisors))
-node_size = Counter(supervisors)
+#node_size = Counter(supervisors)
 
 for person in all_people:
-    net.add_node(person, title=person,size=1)
+    net.add_node(person, title=person,size=5) #,shape='ellipse')
 
 for person, supervisor, job in zip(people,supervisors, jobtype):
     net.add_edge(supervisor, person, arrow=True,color=color[job])
@@ -28,7 +29,10 @@ neighbor_map = net.get_adj_list()
 
 # add neighbor data to node hover data
 for node in net.nodes:
-    node["title"] += " Neighbors:<br>" + "<br>".join(neighbor_map[node["id"]])
-    node["value"] = len(neighbor_map[node["id"]])
+    supervised = [x for x in net.neighbors(node["id"])]
+    num = len(supervised)
+    if num > 0: 
+        node["title"] += " Students:<br>" + "<br>".join(neighbor_map[node["id"]])
+    node["value"] = num 
 
-net.show("themob.html")
+net.show("index.html")
